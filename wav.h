e2f458ab357175c6ef6851f596965b5e9887cec6
root <root@RylansLaptop.>
2025-09-24 18:08:53 -0400

    more correct version of wav.h

--- wav.h
+++ wav.h
@@ -5,19 +5,25 @@
 #include <stddef.h>
 
 typedef struct __attribute__((packed)) {
+    char riff[4]; // will store first 4 bytes; should be RIFF
+    char wav[4]; // will store bytes 8-11; should be WAVE
     unsigned int sampleRate;
-    int channel;
+    int channels; //will store whether file in mono or stereo; should be stereo
     int bitsPerSample;
     WavFile* wavFile;
 } wav_header;
  
 typedef struct __attribute__((packed)) {
-    uint8_t* data
+    wav_header* header;
+    size_t fileSize;
+    uint8_t* data;
 } wav_file;
 
-wav_header setWavHeader(char** wav_file);
+int setWavHeader(char** file, wav_header* wav);
 
 wav_file defineHeaderDetails(char** wav_file);
 
-wav_file reverseWavFile();
+void reverseWavFile(wav_file* wav);
 
+
+#endif
