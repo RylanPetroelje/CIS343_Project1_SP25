9ebda643392c5d27c130f3ccfbdcd9e4ae29070d
Seth Perko <perkos@mail.gvsu.edu>
2025-09-24 18:07:03 -0400

    adds setWavHeader

--- wav.c
+++ wav.c
@@ -1,1 +1,18 @@
-#include <wav.h>
+#include "wav.h"
+
+int setWavHeader(char** file, wav_header* wav) {
+    char* contents;
+    size_t num_bytes = read_file(file, &contents);
+    char* data = contents + 44;
+    size_t data_size = num_bytes - HEADER_SIZE;
+
+    wav.sampleRate = *(int*)(contents + 24);
+    wav.channel = *(contents + 22);
+    wav.bitsPerSample = *(contents + 34);
+
+    printf("Sample Rate: %d\n", wav.sample_rate);
+    printf("Channels: %d\n", wav.channels);
+    printf("Bits per sample: %d\n", wav.bitsPerSample);
+
+    return 1;
+}
