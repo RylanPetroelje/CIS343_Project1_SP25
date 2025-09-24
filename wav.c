d46913ee4ec867fdd88d01981966b82988e7165e
Seth Perko <perkos@mail.gvsu.edu>
2025-09-24 18:24:07 -0400

    adds current work

--- wav.c
+++ wav.c
@@ -3,17 +3,29 @@
 
 #include "wav.h"
 
-int setWavHeader(char** file, wav_header* wav) {
-    char* contents;
-    read_file(file, &contents);
+int setWavHeader(char* contents, wav_header* wav) {
 
     wav.sampleRate = *(int*)(contents + 24);
-    wav.channel = *(contents + 22);
-    wav.bitsPerSample = *(contents + 34);
+    wav.channel = *(int*)(contents + 22);
+    wav.bitsPerSample = *(int*)(contents + 34);
 
-    printf("Sample Rate: %d\n", wav.sample_rate);
+    printf("Sample Rate: %d\n", wav.sampleRate);
     printf("Channels: %d\n", wav.channels);
     printf("Bits per sample: %d\n", wav.bitsPerSample);
 
     return 1;
 }
+
+int defineHeaderDetails(char** file, wav_header* wavHeader, wav_file* wavFile) {
+    char* data;
+    read_file(file, &data);
+
+    setWavHeader(data, wavHeader);
+
+    wavFile.fileSize = read_file(file, &wavFile.data);
+    wavFile.header = wavHeader;
+
+    return 1;
+}
+
+#endif
