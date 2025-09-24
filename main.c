2ff9187934ae6f1d9fc8229de26a83d53da70f3d
Seth Perko <perkos@mail.gvsu.edu>
2025-09-24 17:03:40 -0400

    reverses .wav files

--- main.c
+++ main.c
@@ -7,20 +7,29 @@
 #define HEADER_SIZE 44
 
 int main(int argc, char** argv){
-    // Read the input file
+    char* contents;
     size_t num_bytes = read_file(argv[1], &contents);
+    char* data = contents + HEADER_SIZE;
+    size_t data_size = num_bytes - HEADER_SIZE;
+
     printf("%s\n", argv[1]);
     printf("%lu bytes read.\n", num_bytes);
 
-    char* contents;
-    char* data = contents + WAV_HEADER_SIZE;
-    size_t data_size = num_bytes - WAV_HEADER_SIZE;
+    int num_channels = *(contents + 22);
+    int  bits_per_sample = *(contents + 34);
+    int bytes_per_sample = (bits_per_sample / 8) * num_channels;
+
+    printf("Channels: %u\n", num_channels);
+    printf("Bits per sample: %u\n", bits_per_sample);
+    printf("Bytes per sample (Total): %d\n", bytes_per_sample);
 
     // Reverse the data
-    for (size_t i = 0; i < data_size / 2; i++) {
-        char k = data[i];
-        data[i] = data[data_size - 1 - i];
-        data[data_size - 1 - i] = k;
+    for (size_t i = 0; i < data_size / 2; i += bytes_per_sample) {
+        for (int j = 0; j < bytes_per_sample; j++) {
+            char tmp = data[i + j];
+            data[i + j] = data[data_size - bytes_per_sample - i + j];
+            data[data_size - bytes_per_sample - i + j] = tmp;
+        }
     }
 
     // Write the file to the output
