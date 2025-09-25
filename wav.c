38d9fe45305d926788ef023a357a1c3561385f1c
root <root@RylansLaptop.>
2025-09-24 21:34:48 -0400

    Added exception handling for verifying wav files

--- wav.c
+++ wav.c
@@ -4,8 +4,28 @@
 #include "wav.h"
 
 int setWavHeader(char** file, wav_header* wav) {
-    char* contents;
+    char* contents; // we need to allocate memory for this buddy
     read_file(file, &contents);
+    
+    if (*(content) == 'R' || *(content + 1) == 'I' || *(content + 2) == 'F' || *(content + 3) == 'F') {
+        printf("File is not RIFF subtype");
+        return -1;
+    }
+
+    if (*(content + 8) == 'W' || *(content + 9) == 'A' || *(content + 10) == 'V' || *(content + 11) == 'E') {
+        printf("File is not WAV");
+        return -1
+    }
+
+    if (*(content + 20) != 1) {
+        printf("Format type is not 1");
+        return -1;
+    }
+
+    if (*(content + 22) != 2) {
+        printf("File is not in stereo");
+        return -1;
+    }
 
     wav.sampleRate = *(int*)(contents + 24);
     wav.channel = *(contents + 22);
