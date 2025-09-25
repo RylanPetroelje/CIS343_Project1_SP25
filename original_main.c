3f390feba54194db182ff3e69f07efdc999768ab
Seth Perko <perkos@mail.gvsu.edu>
2025-09-25 15:22:47 -0400

    does SOME fixing

--- original_main.c
+++ original_main.c
@@ -6,8 +6,21 @@ int main(int argc, char** argv){
     wav_header wavHeader;
     wav_file wavFile;
 
+    printf("Input File Name: %s\n", argv[1]);
+    printf("Output File Name: %s\n", argv[2]);
+
     defineHeaderDetails(argv[1], &wavHeader, &wavFile);
+
+    printf("File Size: %lu\n", wavFile.fileSize);
+    printf("Sample Rate: %d\n", wavHeader.sampleRate);
+    printf("Channels: %d\n", wavHeader.channels);
+    printf("Bits per sample: %d\n\n", wavHeader.bitsPerSample);
+    printf("Starting Reversal...\n");
+
     reverseWavFile(&wavFile, argv[2]);
+
+    printf("Done Reversing!\n");
+    printf("Reversed file written to %s\n", argv[2]);
 
     free(wavFile.data); // Always free allocated memory
     return 0;
--- wav.c
+++ wav.c
@@ -5,33 +5,33 @@
 
 int setWavHeader(char* contents, wav_header* wav) {
 
-    if (*(content) == 'R' || *(content + 1) == 'I' || *(content + 2) == 'F' || *(content + 3) == 'F') {
+    if (*(contents) == 'R' || *(contents + 1) == 'I' || *(contents + 2) == 'F' || *(contents + 3) == 'F') {
         printf("File is not RIFF subtype");
         return -1;
     }
 
-    if (*(content + 8) == 'W' || *(content + 9) == 'A' || *(content + 10) == 'V' || *(content + 11) == 'E') {
+    if (*(contents + 8) == 'W' || *(contents + 9) == 'A' || *(contents + 10) == 'V' || *(contents + 11) == 'E') {
         printf("File is not WAV");
-        return -1
+        return -1;
     }
 
-    if (*(content + 20) != 1) {
+    if (*(contents + 20) != 1) {
         printf("Format type is not 1");
         return -1;
     }
 
-    if (*(content + 22) != 2) {
+    if (*(contents + 22) != 2) {
         printf("File is not in stereo");
         return -1;
     }
 
-    wav.sampleRate = *(int*)(contents + 24);
-    wav.channel = *(int*)(contents + 22);
-    wav.bitsPerSample = *(int*)(contents + 34);
+    wav->sampleRate = *(int*)(contents + 24);
+    wav->channels = *(int*)(contents + 22);
+    wav->bitsPerSample = *(int*)(contents + 34);
 
-    printf("Sample Rate: %d\n", wav.sampleRate);
-    printf("Channels: %d\n", wav.channels);
-    printf("Bits per sample: %d\n", wav.bitsPerSample);
+    printf("Sample Rate: %d\n", wav->sampleRate);
+    printf("Channels: %d\n", wav->channels);
+    printf("Bits per sample: %d\n", wav->bitsPerSample);
 
     return 1;
 }
@@ -42,8 +42,8 @@ int defineHeaderDetails(char* file, wav_header* wavHeader, wav_file* wavFile) {
 
     setWavHeader(data, wavHeader);
 
-    wavFile.fileSize = read_file(file, &wavFile.data);
-    wavFile.header = wavHeader;
+    wavFile->fileSize = read_file(file, &wavFile->data);
+    wavFile->header = wavHeader;
 
     return 1;
 }
